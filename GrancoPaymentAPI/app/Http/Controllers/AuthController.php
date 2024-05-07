1948bc218730663abcf7d4fab8428a6fb7caaf97
GBKbala <balakannangbk01@gmail.com>
2024-05-07 18:25:21 +0530

    :art: Added GrandcoAPI new code

---
+++ GrancoPaymentAPI/app/Http/Controllers/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
+use DB;
+use JWTAuth;
+
+class AuthController extends Controller
+{
+
+    public function __construct(){
+
+    }
+
+    public function login( Request $request){
+        $rules = [
+            "CustomerID" => "required",
+        ];
+        $validate = Validator::make($request->all(), $rules);
+        if($validate->fails()){
+            $result = new Response([
+                "status" => "error",
+                "message" => $validate->errors()
+            ]);
+            $result->setStatusCode(422);
+        } else {
+            try {
+                $data = [];
+                $customerID = $request->input('CustomerID');
+                $devices = DB::connection('mysql')
+                    ->table('devices')
+                    ->where('deviceAuthID', '=', $customerID);
+                if($devices->count() > 0){
+                    $token = $this->generateJWT($devices->first());
+                    // $cookie = cookie('jwt', $token, 60*24);
+
+                    $data['access_token'] = $token;
+                    $data['status'] = 'success';
+                    $data['message'] = 'Device token created successfully !!';
+
+                    $result = new Response($data);
+                    $result->setStatusCode(200);
+                }else{
+                    throw new \Exception('Invalid device');
+                }
+            } catch (\Throwable $th) {
+                $result = new Response([
+                    "status" => "error",
+                    "message" => $th->getMessage()
+                ]);
+                $result->setStatusCode(500);
+            }
+        }
+        return $result;
+    }
+
+
+
+
+}
---
+++ GrancoPaymentAPI/app/Http/Middleware/JWTMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use JWTAuth;
+use Session;
+class JWTMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+            $payload = JWTAuth::parseToken()->getPayload();
+            Session::put('JWT', $payload);
+            // $request->attributes->add(['jwtPayload' => $payload]);
+            // dd($request);
+        } catch (TokenInvalidException $e) {
+            return response()->json(['error' => 'Invalid token', 'status' => 'error', 'message' => 'Invalid token'], 401);
+        } catch (TokenExpiredException $e) {
+            return response()->json(['error' => 'Token expired', 'status' => 'error', 'message' => 'Token expired'], 401);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Token not found', 'status' => 'error', 'message' => 'Token not found'], 401);
+        }
+        return $next($request);
+    }
+}
