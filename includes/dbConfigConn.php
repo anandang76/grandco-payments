e51b11d31b42b56472c03731effad5cb4586cdf3
GBKbala <balakannangbk01@gmail.com>
2024-05-07 14:48:47 +0530

    :fire: Removed unwanted files

--- includes/dbConfigConn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbUser="aqmsConfig".$configCompanyID;;
-$dbPass="Bgt56yhN@";
-$dbHost="localhost";
-$dbConfig="aqmsConfig".$configCompanyID;
-$dbConfigConn=new mysqli($dbHost,$dbUser,$dbPass,$dbConfig) or die("Error Connect Config ".mysqli_error($dbConfigConn));
-
-?>
--- includes/dbRptConn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbUser="aqmsReport".$configCompanyID;;
-$dbPass="Bgt56yhN@";
-$dbHost="localhost";
-$dbRpt="aqmsReport".$configCompanyID;
-$dbRptConn=new mysqli($dbHost,$dbUser,$dbPass,$dbRpt) or die("Error Connect Config ".mysqli_error($dbRptConn));
-
-?>
